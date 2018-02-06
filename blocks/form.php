28256c288c98c6f17f4b9ea6ad69ca34e65f9949
Thoriq Firdaus <thoriqoe@gmail.com>
2018-02-06 21:14:46 +0700

    Block Init

---
+++ blocks/form.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Functions to register client-side assets (scripts and stylesheets) for the
+ * Gutenberg block.
+ *
+ * @package wp-chimp
+ */
+
+/**
+ * Registers all block assets so that they can be enqueued through Gutenberg in
+ * the corresponding context.
+ *
+ * @see https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type/#enqueuing-block-scripts
+ */
+function form_block_init() {
+	$dir = dirname( __FILE__ );
+
+	$block_js = 'form/block.js';
+	wp_register_script(
+		'form-block-editor',
+		plugins_url( $block_js, __FILE__ ),
+		array(
+			'wp-blocks',
+			'wp-i18n',
+			'wp-element',
+		),
+		filemtime( "$dir/$block_js" )
+	);
+
+	$editor_css = 'form/editor.css';
+	wp_register_style(
+		'form-block-editor',
+		plugins_url( $editor_css, __FILE__ ),
+		array(
+			'wp-blocks',
+		),
+		filemtime( "$dir/$editor_css" )
+	);
+
+	$style_css = 'form/style.css';
+	wp_register_style(
+		'form-block',
+		plugins_url( $style_css, __FILE__ ),
+		array(
+			'wp-blocks',
+		),
+		filemtime( "$dir/$style_css" )
+	);
+
+	register_block_type( 'wp-chimp/form', array(
+		'editor_script' => 'form-block-editor',
+		'editor_style'  => 'form-block-editor',
+		'style'         => 'form-block',
+	) );
+}
+add_action( 'init', 'form_block_init' );
