5240859da552c446ca0db266ed7a15ad0e486a9d
Thoriq Firdaus <thoriqoe@gmail.com>
2018-04-02 17:55:31 +0700

    Load the function helpers and utilities

--- includes/class-plugin.php
+++ includes/class-plugin.php
@@ -121,6 +121,11 @@ class Plugin {
 	private function load_dependencies() {
 
 		/**
+		 * Helpers and utility functions of the core plugin.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/functions.php';
+
+		/**
 		 * The class responsible for orchestrating the actions and filters of the
 		 * core plugin.
 		 */
