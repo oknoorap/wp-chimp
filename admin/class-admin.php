de1f439ade43b5c6ea3d24aa6fb754f1c855f3ee
Thoriq Firdaus <thoriqoe@gmail.com>
2018-04-11 22:00:01 +0700

    Change class name in the comments

--- admin/class-admin.php
+++ admin/class-admin.php
@@ -85,10 +85,10 @@ class Admin {
 			 * This function is provided for demonstration purposes only.
 			 *
 			 * An instance of this class should be passed to the run() function
-			 * defined in WP_Chimp_Loader as all of the hooks are defined
+			 * defined in WP_Chimp/Loader as all of the hooks are defined
 			 * in that particular class.
 			 *
-			 * The WP_Chimp_Loader will then create the relationship
+			 * The WP_Chimp/Loader will then create the relationship
 			 * between the defined hooks and the functions defined in this
 			 * class.
 			 */
@@ -111,10 +111,10 @@ class Admin {
 			 * This function is provided for demonstration purposes only.
 			 *
 			 * An instance of this class should be passed to the run() function
-			 * defined in WP_Chimp_Loader as all of the hooks are defined
+			 * defined in WP_Chimp/Loader as all of the hooks are defined
 			 * in that particular class.
 			 *
-			 * The WP_Chimp_Loader will then create the relationship
+			 * The WP_Chimp/Loader will then create the relationship
 			 * between the defined hooks and the functions defined in this
 			 * class.
 			 */
