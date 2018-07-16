324971a3fe40460a2ab1c5b4389865295983c0ef
Thoriq Firdaus <thoriqoe@gmail.com>
2018-07-16 23:57:05 +0700

    Align version number discrepancies

--- core/class-plugin.php
+++ core/class-plugin.php
@@ -142,8 +142,8 @@ class Plugin {
 	protected function check_requirements() {
 
 		$requires = [
-			'php' => '5.4',
-			'wp' => '4.9',
+			'php' => '5.4.45',
+			'wp' => '4.9.0',
 			'wp_cron' => true,
 		];
 		$this->requirements = new WP_Chimp_Requirements( 'WP Chimp', $requires );
